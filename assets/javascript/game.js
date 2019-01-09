750ad66d8ce643c3759fc0023ad516152070f9e8
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-01-08 18:16:27 -0800

    asdf

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,28 @@
 $(document).ready( function() {
-    
+var theirPoints = 0;
 var win=0;
 var lose=0;
-
+var randIntOne;
+var randIntTwo;
+var randIntThree;
+var randIntFour;
 $("#img1").on("click", function() {
-alert("clicked");
+theirPoints = theirPoints + randIntOne;
+console.log(theirPoints);
 });
-console.log("js file linked");
 
 
+//assigning point values to 4 crystals
+function assignValues() {
+    var randIntOne = Math.floor(Math.random() * 10);
+    var randIntTwo = Math.floor(Math.random() * 10);
+    var randIntThree = Math.floor(Math.random() * 10);
+    var randIntFour = Math.floor(Math.random() * 10);
+
+};
+
+assignValues();
+console.log(randIntFour);
 
 
 
