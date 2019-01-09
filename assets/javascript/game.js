50e0d14579047dce7463d78d0d56be62d4559ae3
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-01-08 18:04:42 -0800

    document ready added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-
+$(document).ready( function() {
 var win=0;
 var lose=0;
-$("img1").on('click', function() {
-$("#win").html(win);
-alert('clicked');
 
+$("#img1").on("click", function() {
+alert("clicked");
+});
+console.log("js file linked");
 });
